5cff2c34e879d62a3d007aefb92f2f3052db3de4
Baserfaz <3edc87YTgithub>
2018-08-26 19:01:33 +0300

    Added panel java file.

---
+++ src/com/ui/Panel.java
@@ -0,0 +1,100 @@
+package com.ui;
+
+import com.enumerations.InteractAction;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Panel extends InteractableGuiElement {
+
+    private Color backgroundColor;
+    private Color borderColor = Color.white;
+    private boolean drawBorders;
+
+    private List<GuiElement> elements = new ArrayList<>();
+
+    public Panel(int x, int y, int width, int height, Color bgColor, boolean borders,
+                 InteractAction onClickAction, InteractAction onHoverAction) {
+        super(x, y, width, height, onClickAction, onHoverAction);
+        this.backgroundColor = bgColor;
+        this.drawBorders = borders;
+    }
+
+    public Panel(int x, int y, int w, int h, Color bgColor, boolean borders) {
+        super(x, y, w, h, null, null);
+        this.backgroundColor = bgColor;
+        this.drawBorders = borders;
+    }
+
+    public List<GuiElement> getElements() {
+        return elements;
+    }
+
+    @Override
+    public void render(Graphics g) {
+        if(this.isVisible()) {
+
+            // draw rect
+            g.setColor(backgroundColor);
+            g.fillRect(x, y, width, height);
+
+            // draw borders
+            if (drawBorders) {
+                g.setColor(borderColor);
+                g.drawRect(x, y, width, height);
+            }
+
+            // render all elements inside this panel
+            for(GuiElement e : elements) {
+                e.render(g);
+            }
+
+        }
+    }
+
+    public void addElement(GuiElement e) {
+        this.elements.add(e);
+    }
+
+    public void removeElement(GuiElement e) {
+        this.elements.remove(e);
+    }
+
+    @Override
+    public void tick() {
+        for(GuiElement e : elements) {
+            e.tick();
+        }
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+    }
+
+    @Override
+    public void onHover() {
+        super.onHover();
+    }
+
+    @Override
+    public InteractAction getOnClickAction() {
+        return super.getOnClickAction();
+    }
+
+    @Override
+    public void setOnClickAction(InteractAction onClickAction) {
+        super.setOnClickAction(onClickAction);
+    }
+
+    @Override
+    public InteractAction getOnHovertAction() {
+        return super.getOnHovertAction();
+    }
+
+    @Override
+    public void setOnHovertAction(InteractAction onHovertAction) {
+        super.setOnHovertAction(onHovertAction);
+    }
+}
