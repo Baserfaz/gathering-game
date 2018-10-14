a995b1eaa81171d4a866cd15a0a21eeebc890d59
heikki <heikkiheiskanen@hotmail.com>
2018-10-14 13:45:45 +0300

    Recursive check panels when hovering & clicking

--- src/com/engine/MouseInput.java
+++ src/com/engine/MouseInput.java
@@ -18,11 +18,14 @@ import com.utilities.Util;
 public class MouseInput implements MouseMotionListener, MouseListener {
     
     private GuiElement lastElementHovered;
-    
+    private boolean hoveredOnSomething;
+
     public void mousePressed(MouseEvent e) {}
 
     public void mouseReleased(MouseEvent e) {
-        
+
+        if(e.getButton() != MouseEvent.BUTTON1) return;
+
         Point mousePos = Game.instance.getMousePos();
         GameState state = Game.instance.getGamestate();
         List<Panel> panels = Util.getPanelsInGamestate(state);
@@ -30,36 +33,38 @@ public class MouseInput implements MouseMotionListener, MouseListener {
         if(panels.isEmpty() || mousePos == null) return;
         
         for(Panel panel : panels) {
-
-            // panels elements
-            for(GuiElement el : panel.getElements()) {
-
-                if(el.getBounds().contains(mousePos) == false) {
-                    // we didn't click this element!
-                    // -> call unfocus
-                    if(el instanceof InteractableGuiElement) {
-                        InteractableGuiElement iel = (InteractableGuiElement) el;
-                        iel.onUnfocus();
-                    }
-                    continue;
-                }
-
-                if(el.isEnabled() == false) continue;
-                if(el instanceof InteractableGuiElement == false) continue;
-
-                InteractableGuiElement iel = (InteractableGuiElement) el;
-
-                if(Game.isMuted == false && panel.isMuted() == false && el.isMuted() == false) {
-                    Game.instance.getSoundManager().playSound(SoundEffect.SELECT);
-                }
-
-                iel.onClick();
-                break;
-            }
+            handlePanelElements(panel, mousePos);
         }
     }
-    
 
+    // go through panel's child panels too recursively
+    private void handlePanelElements(Panel panel, Point mousePos) {
+        for(GuiElement el : panel.getElements()) {
+            if(el instanceof Panel) { this.handlePanelElements((Panel) el, mousePos); }
+            if(handleClick(panel, el, mousePos)) break;
+        }
+    }
+
+    private boolean handleClick(Panel panel, GuiElement el, Point mousePos) {
+        if(el.getBounds().contains(mousePos) == false) {
+            // we didn't click this element -> call unfocus
+            if(el instanceof InteractableGuiElement) {
+                InteractableGuiElement iel = (InteractableGuiElement) el;
+                iel.onUnfocus();
+            }
+            return false;
+        }
+
+        if(el.isEnabled() == false || el instanceof InteractableGuiElement == false) return false;
+        InteractableGuiElement iel = (InteractableGuiElement) el;
+
+        if(Game.isMuted == false && panel.isMuted() == false && el.isMuted() == false) {
+            Game.instance.getSoundManager().playSound(SoundEffect.SELECT);
+        }
+
+        iel.onClick();
+        return true;
+    }
 
     // hover effects on gui elements.
     public void mouseMoved(MouseEvent e) {
@@ -68,35 +73,10 @@ public class MouseInput implements MouseMotionListener, MouseListener {
         if(Game.instance.getGuiElementManager() == null) return;
         List<Panel> panels = Util.getPanelsInGamestate(Game.instance.getGamestate());
         if(panels.isEmpty()) return;
-        
-        boolean hoveredOnSomething = false;
-        
-        for(Panel panel : panels) {
 
-            // if(panel.isEnabled() == false ) { continue; }
-            // if(panel.getBounds().contains(e.getPoint()) == false) { continue; }
+        this.hoveredOnSomething = false;
 
-            // panels elements
-            for(GuiElement el : panel.getElements()) {
-
-                if(el instanceof InteractableGuiElement == false) continue;
-                if(el.isEnabled() == false) { continue; }
-                if(el.getBounds().contains(e.getPoint()) == false) { continue; }
-
-                InteractableGuiElement iel = (InteractableGuiElement) el;
-
-                hoveredOnSomething = true;
-
-                if(this.lastElementHovered != el && Game.isMuted == false && panel.isMuted() == false && el.isMuted() == false) {
-                    Game.instance.getSoundManager().playSound(SoundEffect.HOVER);
-                }
-
-                iel.setHovering(true);
-                iel.onHover();
-                this.lastElementHovered = el;
-                break;
-            }
-        }
+        for(Panel panel : panels) { if(handleHoverOnPanelElements(panel, e)) break; }
 
         if(hoveredOnSomething == false && this.lastElementHovered != null) {
             if(this.lastElementHovered instanceof InteractableGuiElement) {
@@ -105,6 +85,33 @@ public class MouseInput implements MouseMotionListener, MouseListener {
             }
             this.lastElementHovered = null;
         }
+    }
+
+    private boolean handleHoverOnPanelElements(Panel panel, MouseEvent e) {
+        // panels elements
+        for(GuiElement el : panel.getElements()) {
+            if(el instanceof Panel) { this.handleHoverOnPanelElements((Panel) el, e); }
+            if(this.handleHoverOnElement(panel, el, e)) return true;
+        }
+        return false;
+    }
+
+    private boolean handleHoverOnElement(Panel panel, GuiElement el, MouseEvent e) {
+        if(el instanceof InteractableGuiElement == false ||
+                el.isEnabled() == false ||
+                el.getBounds().contains(e.getPoint()) == false) { return false; }
+
+        InteractableGuiElement iel = (InteractableGuiElement) el;
+        this.hoveredOnSomething = true;
+
+        if(this.lastElementHovered != el && Game.isMuted == false && panel.isMuted() == false && el.isMuted() == false) {
+            Game.instance.getSoundManager().playSound(SoundEffect.HOVER);
+        }
+
+        iel.setHovering(true);
+        iel.onHover();
+        this.lastElementHovered = el;
+        return true;
     }
 
     public void mouseDragged(MouseEvent e) { Game.instance.setMousePos(e.getPoint()); }
